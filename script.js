40bd1a11a382576688dd33d4593600c742ad16c7
Andra <andra.popa1989@gmail.com>
2019-01-11 14:26:42 +0000

    add axisBottom

--- script.js
+++ script.js
@@ -13,27 +13,40 @@ const dataset = req;
 
 const w = 800,
       h = 400,
-      padding = w / req.length;
+      yMargin = 40,
+      padding = w / dataset.length;
 
 const svg = d3.select('body')
             .append('svg')
+            .attr('class', 'chart')
             .attr('width', w)
             .attr('height', h);
 
+const xScale = d3.scaleLinear()
+                .domain([0, dataset.length])
+                .range([padding, w - padding]);
+
 const yScale = d3.scaleLinear()
                 .domain([d3.max(dataset, (d) => d[1]), 0])
-                .range([h, 0]);
-
+                .range([h - yMargin, 0]);
+    
 svg.selectAll('rect')
     .data(dataset)
     .enter()
     .append('rect')
     .attr('class', 'bar')
     .attr('x', (d, i) => i * padding) 
-    .attr('y', (d) => h - yScale(d[1]))
+    .attr('y', (d) => h - yMargin - yScale(d[1]))
     .attr('width', 5)
     .attr('height', (d) => yScale(d[1]))
     .attr('fill', '#333')
     .append('title')
-    .text((d) => d)
+    .text((d) => d);
+
+const xAxis = d3.axisBottom(xScale)
+                .ticks(10);
+
+svg.append('g')
+    .attr('transform', 'translate(0, ' + (h - 40) + ')')
+    .call(xAxis);
 }
